fdf31867c91a7484169e268b334f999bae907edc
Nasser AlQimlas <naser.alqimlas@gmail.com>
2023-04-10 03:16:36 +0300

    createSSGHelpers and getting user data to profile page

--- src/server/api/routers/posts.ts
+++ src/server/api/routers/posts.ts
@@ -1,4 +1,3 @@
-import type { User } from "@clerk/nextjs/dist/api";
 import clerkClient from "@clerk/clerk-sdk-node";
 import { z } from "zod";
 
@@ -9,16 +8,9 @@ import {
 } from "~/server/api/trpc";
 import { TRPCError } from "@trpc/server";
 
-const filterUserForClient = (user: User) => {
-  return {
-    id: user.id,
-    username: user.username,
-    profileImageUrl: user.profileImageUrl,
-  };
-};
-
 import { Ratelimit } from "@upstash/ratelimit"; // for deno: see above
 import { Redis } from "@upstash/redis";
+import { filterUserForClient } from "~/server/helpers/filterUserForClient";
 
 // Create a new ratelimiter, that allows 3 requests per 1 minute
 const ratelimit = new Ratelimit({
